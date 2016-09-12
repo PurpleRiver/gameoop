4f0856074c914a2ad04e9f2f0ffed5f1e8fcc0b4
PurpleRiver <idel.yusupoff@yandex.ru>
2016-09-12 19:11:28 +0500

    debugging

--- gameoop/GiantGolem.swift
+++ gameoop/GiantGolem.swift
@@ -1,3 +1,14 @@
 
-
 import Foundation
+
+class GiantGolem: Enemy {
+    
+    override var loot: [String] {
+        return ["Stone Hearht", "Essence of golem"]
+        
+    }
+    override var type: String {
+        return "Giant Golem"
+    }
+    
+}
--- gameoop/ViewController.swift
+++ gameoop/ViewController.swift
@@ -4,19 +4,89 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var player: Player!
+    var enemy: Enemy!
+    var chestMessage: String?
+    var inventory: [String]?
     
     
+    @IBOutlet weak var playerImg: UIImageView!
+    
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBOutlet weak var attackImg: UIButton!
 
+    @IBOutlet weak var chestImg: UIButton!
+    
+    @IBOutlet weak var printLbl: UILabel!
+    
+    @IBOutlet weak var playerHp: UILabel!
+    
+    @IBOutlet weak var enemyHp: UILabel!
+    
+    @IBOutlet weak var chestMsg: UILabel!
+    
+    @IBOutlet weak var inventoryLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "Somename", hp: 50, attackPwr: 50)
+        
+        playerHp.text = "\(player.hp) HP"
+        
+        generateRandomEnemy()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func generateRandomEnemy () {
+        let rand = Int(arc4random_uniform(2))
+        
+            if rand == 0 {
+                enemy = DevilWizard(startingHp: 100, attackPwr: 20)
+            }else {
+                enemy = GiantGolem(startingHp: 100, attackPwr: 10)
+            }
+        enemyImg.hidden = false
+            
+        }
+    
+    @IBAction func btnPressed(sender: AnyObject) {
+        
+        if enemy.attempAttack(player.attackPwr) && player.attempAttack(enemy.attackPwr) {
+            printLbl.text = "Attaked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHp.text = "\(enemy.hp) HP"
+            playerHp.text = "\(player.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            inventoryLbl.text = "\(player.inventory)"
+            chestMessage = "\(player.name) found \(loot)"
+            chestImg.hidden = false
+            
+        }
+        if !enemy.isAlive {
+            enemyHp.text = ""
+            enemyImg.hidden = true
+            printLbl.text = "Killed \(enemy.type)"
+        }
+        if !player.isAlive {
+            playerHp.text = ""
+            playerImg.hidden = true
+            chestMsg.text = "Killed by \(enemy.type)"
+            printLbl.text = "GAME OVER!"
+            attackImg.hidden = true
+        }
+      
     }
-
-
+    
+    @IBAction func chestBtn(sender: AnyObject) {
+        
+        chestImg.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.generateRandomEnemy), userInfo: nil, repeats: false)
+    }
+    
 }
 
